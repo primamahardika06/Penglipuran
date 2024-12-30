4b3f1a246a1941ba3b4c83cdf67def5d357c25da
primamahardika06 <primaodut06@gmail.com>
2024-12-31 00:23:51 +0800

    mobile responsive

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnGopay = document.querySelector(".gopay button");
   const btnDana = document.querySelector(".dana button");
 
+
   btnBca.addEventListener("click", function () {
     btnBca.classList.add("active");
     btnMandiri.classList.remove("active");
@@ -44,6 +45,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const btnDomestik = document.querySelector(".domestik button");
+  const btnInternasional = document.querySelector(".internasional button");
+
+  btnDomestik.addEventListener("click", function () {
+    btnDomestik.classList.add("active");
+    btnInternasional.classList.remove("active");
+  });
+  btnInternasional.addEventListener("click", function () {
+    btnInternasional.classList.add("active");
+    btnDomestik.classList.remove("active");
+  });
+
+
+})
+
 // Movetext
 const moveText = document.querySelector(".move_text h1");
 document.addEventListener("scroll", function(){
@@ -62,23 +79,23 @@ const scrollLeft = document.querySelector(".bRight");
 scrollLeft.addEventListener("click", function(e){
   var cardWidth = document.querySelector(".slider_content .card").offsetWidth;
   sliderContent.scrollBy({
-    left: cardWidth + 24,  // scroll ke kanan
-    behavior: 'smooth'     // memberikan animasi mulus
+    left: cardWidth + 24,
+    behavior: 'smooth'    
   });
 });
 
 scrollRight.addEventListener("click", function(e){
   var cardWidth = document.querySelector(".slider_content .card").offsetWidth;
   sliderContent.scrollBy({
-    left: -(cardWidth + 24),  // scroll ke kiri
-    behavior: 'smooth'        // memberikan animasi mulus
+    left: -(cardWidth + 24),  
+    behavior: 'smooth'        
   });
 });
 
 
 // pop up message
 // Ambil elemen-elemen yang dibutuhkan
-const buyNowButton = document.querySelector('.detail_payment button');
+const buyNowButton = document.querySelector('.payment_button button');
 const popMessage = document.querySelector('.pop_message1');
 const continueButton = document.querySelector('.pop_up1 button');
 const inputs = document.querySelectorAll('.detail_payment input');
@@ -150,4 +167,61 @@ closeReviewButton.addEventListener('click', () => {
 
 
 
+// sticky navbar start
 
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+  const stickyThreshold = 500;
+
+  function makeNavbarSticky() {
+      if (window.pageYOffset >= stickyThreshold) {
+          navbar.classList.add("sticky");
+      } else {
+          navbar.classList.remove("sticky");
+      }
+  }
+
+  window.onscroll = function () {
+      makeNavbarSticky();
+  };
+});
+// sticky navbar end
+
+
+// Hamburger Start
+
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navHamburger = document.querySelector(".nav_hamburger");
+
+  hamburger.addEventListener("click", function(event) {
+      event.stopPropagation();
+
+      if (navHamburger.style.right === "0px") {
+          navHamburger.style.right = "-100%";
+      } else {
+          navHamburger.style.right = "0";
+      }
+
+      hamburger.classList.toggle("active");
+  });
+
+  document.addEventListener("click", function(event) {
+      if (!navHamburger.contains(event.target) && !hamburger.contains(event.target)) {
+          navHamburger.style.right = "-100%";
+          hamburger.classList.remove("active");
+      } else if (navHamburger.contains(event.target) && event.target.tagName !== 'A' && event.target.tagName !== 'BUTTON') {
+          navHamburger.style.right = "-100%";
+          hamburger.classList.remove("active");
+      }
+  });
+
+  window.addEventListener("resize", function() {
+      if (window.innerWidth > 768) {
+          navHamburger.style.right = "-100%";
+          hamburger.classList.remove("active");
+      }
+  });
+});
+
+// Hamburger End
